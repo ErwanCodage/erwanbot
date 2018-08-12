b1b7d478a2364ca0cba00e117a4df5e9bf7c1b72
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-12 15:04:53 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,7 +14,7 @@ bot.login('NDY4MTQwNTM4NTM3OTY3NjI2.Di49Gw._SlpwQY-RWE9r1hn5iBCs_r4NmE');
 bot.on('message', message => {
 
   if (message.content === prefix 'ping') {
-   message.channel.send(`**${message.author.username} Pong! \nLa latence est** ${msg.createdTimestamp - message.createdTimestamp}ms.** La latence de l'API est **${Math.round(bot.ping)}ms `)
+   message.channel.send(`**${message.author.username} Pong! \nLa latence est** ${msg.createdTimestamp - message.createdTimestamp}ms.** La latence de l'API est **${Math.round(client.ping)}ms `)
    
   }
 
