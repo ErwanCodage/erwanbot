d6a626e225c5951c553679e000d1777610cc6ad9
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-12 17:15:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("*")
+var prefix = ("--")
 
 bot.on('ready', function() {
     bot.user.setGame("Command: :help");
@@ -12,17 +12,10 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n !tt \n !fabriquant");
+    if (message.content === prefix + "test"){
+        message.channel.sendMessage("test reusi");
     }
 
-    if (message.content === prefix + "tt"){
-        message.channe("Création du bot le _11/12/2017_ à _13h57_");
-        console.log("Commande effectué");
-    }
+ 
 
-    if (message.content === prefix + "fabriquant"){
-        message.reply("_Ce bot a été crée par MhBurgerKing_");
-        console.log("Commande effectué");
-    }
 });
