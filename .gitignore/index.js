f3e5bcb6606d74a4713ff43fe0cf33e05456d68a
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-12 15:53:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,51 +18,10 @@ bot.login('NDY4MTQwNTM4NTM3OTY3NjI2.Di49Gw._SlpwQY-RWE9r1hn5iBCs_r4NmE');
 bot.on('message', message => {
 
 
-  	   if(message.content == '--servinfo') {
- 	
-message.channel.send({embed: {
-    color: 3248003,
-    title: "Info du serveur",
-    description: "Information du Serveur",
-    fields: [{
-        name: "Creations Du Serveur/Date",
-        value: ` ${day}.${month}.${year} `
-      },
-	  {
-        name: `Fondateur`,
-        value:`${owner.username || 'None'} `
-      },
-	  
-      {
-        name: "Onligne-Total",
-        value: `**     ${msg.guild.members.filter(m => m.presence.status !== 'offline').size}    /    ${msg.guild.memberCount}**`
-      },
-	   {
-        name: "Channel",
-        value:  `**${message.guild.channels.filter(chan => chan.type === 'voice').size} Vocal / ${message.guild.channels.filter(chan => chan.type === 'text').size} Textuel**`
-      },
-	  {
-         name: 'Roles',
-         value: `${msg.guild.roles.size}`,
-       },
-	   {
-        name: `Regions`,
-        value:`***${msg.guild.region}*** `
-      }
-	
-	  
-	]
-	  
-	  
+   if (message.content === prefix + "ping"){
+   message.channel.send("Test")
+
    }
-});  
-  
-	   }
-
-
-
-
-
 
  //  if (message.content === prefix + "ping"){
  //  message.channel.send(`**${message.author.username} Pong! \nLa latence est** ${msg.createdTimestamp - message.createdTimestamp}ms.** La latence de l'API est **${Math.round(client.ping)}ms`)
