007e7f1039a3b796e5ac535748db19fa0e24e1e8
ErwanCodage <42281990+ErwanCodage@users.noreply.github.com>
2018-08-12 17:50:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,21 +1,17 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-var prefix = ("--")
-
-bot.on('ready', function() {
+client.on('ready', function() {
     client.user.setActivity(`|En codage|Heberger 24h/24|Erwan_ Mon maitre`);
     console.log("Connected");
 });
 
-bot.login(process.env.TOKEN);
-
-
-bot.on('message', message => {
-    if (message.content === prefix + "test"){
-        message.channel.sendMessage("test reusi");
-    }
-
- 
-
+client.on('ready', () => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
